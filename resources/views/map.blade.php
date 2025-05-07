3535b2e03028c2622e2f454c421263e55542fc41
ekaprameysti <ekaprameysti@gamil.com>
2025-05-08 04:15:09 +0700

    Acara8-DELETE DATA

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -231,10 +231,19 @@
         //GeoJSON Points
         var point = L.geoJson(null, {
             onEachFeature: function(feature, layer) {
+
+                var routedelete = "{{ route('points.destroy', ':id') }}";
+                routedelete = routedelete.replace(':id', feature.properties.id);
+
                 var popupContent = "Nama: " + feature.properties.name + "<br>" +
                     "Deskripsi: " + feature.properties.description + "<br>" +
                     "Dibuat: " + feature.properties.created_at + "<br>" +
-                    "<img src='{{ asset('storage/images/') }}/" + feature.properties.image + "' width='250' alt=''>";
+                    "<img src='{{ asset('storage/images/') }}/" + feature.properties.image + "' width='250' alt=''>" + "<br>" +
+                    "<form method='POST' action='" + routedelete + "'>" +
+                    '@csrf' + '@method("DELETE")' +
+                    "<button type='submit' class='btn btn-sm btn-danger' onclick='return confirm(`Yakin akan dihapus?`)'><i class='fa-solid fa-trash-can'></i></button>"
+                    "</form>";
+
                 layer.on({
                     click: function(e) {
                         point.bindPopup(popupContent);
@@ -253,11 +262,19 @@
         //GeoJSON Polylines
         var polyline = L.geoJson(null, {
             onEachFeature: function(feature, layer) {
+
+                var routedelete = "{{ route('polylines.destroy', ':id') }}";
+                routedelete = routedelete.replace(':id', feature.properties.id);
+
                 var popupContent = "Name: " + feature.properties.name + "<br>" +
                     "Deskripsi: " + feature.properties.description + "<br>" +
                     "Panjang: " + feature.properties.length_km.toFixed(2) + "km<br>" +
                     "Dibuat: " + feature.properties.created_at + "<br>" +
-                    "<img src='{{ asset('storage/images/') }}/" + feature.properties.image + "' width='250' alt=''>";
+                    "<img src='{{ asset('storage/images/') }}/" + feature.properties.image + "' width='250' alt=''>" + "<br>" +
+                    "<form method='POST' action='" + routedelete + "'>" +
+                    '@csrf' + '@method("DELETE")' +
+                    "<button type='submit' class='btn btn-sm btn-danger' onclick='return confirm(`Yakin akan dihapus?`)'><i class='fa-solid fa-trash-can'></i></button>"
+                    "</form>";
                 layer.on({
                     click: function(e) {
                         polyline.bindPopup(popupContent);
@@ -276,11 +293,19 @@
         //GeoJSON Polygons
         var polygon = L.geoJson(null, {
             onEachFeature: function(feature, layer) {
+
+                var routedelete = "{{ route('polygons.destroy', ':id') }}";
+                routedelete = routedelete.replace(':id', feature.properties.id);
+
                 var popupContent = "Name: " + feature.properties.name + "<br>" +
                     "Deskripsi: " + feature.properties.description + "<br>" +
                     "Luas: " + feature.properties.luas_km2.toFixed(2) + "km2<br>" +
                     "Dibuat: " + feature.properties.created_at + "<br>" +
-                    "<img src='{{ asset('storage/images/') }}/" + feature.properties.image + "' width='250' alt=''>";
+                    "<img src='{{ asset('storage/images/') }}/" + feature.properties.image + "' width='250' alt=''>" + "<br>" +
+                    "<form method='POST' action='" + routedelete + "'>" +
+                    '@csrf' + '@method("DELETE")' +
+                    "<button type='submit' class='btn btn-sm btn-danger' onclick='return confirm(`Yakin akan dihapus?`)'><i class='fa-solid fa-trash-can'></i></button>"
+                    "</form>";
                 layer.on({
                     click: function(e) {
                         polygon.bindPopup(popupContent);
